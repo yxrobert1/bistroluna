99a8cd8636a9fa0c38433419c0915fd88ccb168e
yxrobert1 <robertcosma1809@gmail.com>
2025-10-24 20:47:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById("toTop").addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
+
+const textarea = document.getElementById('message');
+const counter = document.getElementById('charCount');
+const max = textarea.getAttribute('maxlength');
+
+textarea.addEventListener('input', () => {
+  const length = textarea.value.length;
+  counter.textContent = `${length} / ${max} caractere`;
+});
